e6431516c1f9b492205d0a2fff5b64ca3616ccc1
Rodrigo Labrador <headstylecolorred@gmail.com>
2025-04-17 11:44:55 +0100

    using tiktoken rust tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use tiktoken_rs::r50k_base;
+
 fn main() {
     // The first step is to load the data from the file
-    let data = std::fs::read_to_string("src/assets/the-verdict.txt").unwrap();
+    let data = std::fs::read_to_string("src/assets/files/the-verdict.txt").unwrap();
 
     // Let's print the first 100 characters of the data
     println!("{}", &data[0..100]);
+
+    // Instantiate the tokenizer
+    let bpe = r50k_base().unwrap();
+
+    // Encode the tokens
+    let tokens = bpe.encode_with_special_tokens(&data);
+
+    // Let's print the number of tokens
+    println!("{}", tokens.len());
+
+    // Let's print the tokens
+    println!("{:?}", tokens);
+
+    // Let's decode the tokens
+    let decoded = bpe.decode(tokens).unwrap();
+
+    // Let's print the decoded tokens
+    println!("{}", decoded);
 }
