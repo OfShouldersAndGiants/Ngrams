5a7488238a990fb13bfc4c432398db95d0b5e8d5
Rodrigo Labrador <headstylecolorred@gmail.com>
2025-04-17 11:26:09 +0100

    correct file ignoring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    // The first step is to load the data from the file
+    let data = std::fs::read_to_string("src/assets/the-verdict.txt").unwrap();
+
+    // Let's print the first 100 characters of the data
+    println!("{}", &data[0..100]);
 }
