150cc23b62bf3a1592ccf91e690a3a292b6a2584
Rodrigo Labrador <headstylecolorred@gmail.com>
2025-04-18 23:47:37 +0100

    slow

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use tiktoken_rs::r50k_base;
+use tiktoken_rs::{r50k_base, Rank};
 
-fn main() {
-    // The first step is to load the data from the file
-    let data = std::fs::read_to_string("src/assets/files/the-verdict.txt").unwrap();
 
-    // Let's print the first 100 characters of the data
-    println!("{}", &data[0..100]);
-
+fn encode_data(data: &str) -> Vec<u32> {
     // Instantiate the tokenizer, we use the r50k_base model because it's the default model for GPT-2
     // and that's the one the book uses
     let bpe = r50k_base().unwrap();
@@ -14,15 +9,34 @@ fn main() {
     // Encode the tokens
     let tokens = bpe.encode_with_special_tokens(&data);
 
-    // Let's print the number of tokens
-    println!("{}", tokens.len());
+    // Return the tokens
+    tokens
+}
 
-    // Let's print the tokens
-    println!("{:?}", tokens);
+fn decode_data(tokens: Vec<Rank>) -> String {
+    let bpe = r50k_base().unwrap();
 
-    // Let's decode the tokens
-    let decoded = bpe.decode(tokens).unwrap();
+    let data: String = bpe.decode(tokens).unwrap();
 
-    // Let's print the decoded tokens
-    println!("{}", decoded);
+    data
+}
+
+fn main() {
+    // The first step is to load the data from the file
+    let data = std::fs::read_to_string("src/assets/files/the-verdict.txt").unwrap();
+
+
+
+    let sample: &str = &data[50..data.len()];
+
+    let encoded_sample = encode_data(sample);
+
+    for i in 1..=encoded_sample.len() {
+        let context = &encoded_sample[..i];
+        let desired = encoded_sample[i];
+
+        // Print the context and desired token
+        println!("{:?} ---> {}", decode_data(context.to_vec()), decode_data([desired].to_vec()));
+    }
+
 }
