6316462ab8c4b4e2b4a3eccc3f1e721567a91a4d
Rodrigo Labrador <headstylecolorred@gmail.com>
2025-04-25 19:05:13 +0100

    correct sampling

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@ mod sampling;
 use sampling::GPTDataset;
 
 fn main() {
-    // Load the data from the file
+    // Load the data from the file into memory
     let data = std::fs::read_to_string("src/assets/files/the-verdict.txt").unwrap();
 
-    let dataset = GPTDataset::new(&data, 1024, 1024).unwrap();
+    // Let's create a dataset with a small context size (4) that matches the book example
+    // and where we use a stride of one token
+    let dataset = GPTDataset::new(&data, 4, 1).unwrap();
 
-    println!("Dataset length: {}", dataset.len());
-    println!("Dataset sample: {:?}", dataset.get(0));
-    let sample = dataset.get(0).unwrap();
-    println!("Sample input: {:?}", sample.0);
-    println!("Sample target: {:?}", sample.1);
+    println!("\nFirst sample:");
+    if let Some(first_sample) = dataset.get(0) {
+        println!("Input token IDs:  {:?}", first_sample.0);
+        println!("Target token IDs: {:?}", first_sample.1);
+    }
+
+    println!("\nSecond sample:");
+    if let Some(second_sample) = dataset.get(1) {
+        println!("Input token IDs:  {:?}", second_sample.0);
+        println!("Target token IDs: {:?}", second_sample.1);
+    }
 }
